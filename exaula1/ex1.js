a0ebfb3cb914cce7e366d07f66eaa3f568f817e1
henriqueelima <106202820+henriqueelima@users.noreply.github.com>
2022-09-02 22:01:32 -0300

    1 file modified, 4 added, 2 deleted

---
+++ exaula1/ex1.js
@@ -0,0 +1,13 @@
+function valores() {
+	const valor1;
+	let cartaodaloja = true;
+
+  const desconto1 = valor1 * 0.15;
+  const desconto2 = valor1 * 0.10;
+
+if (cartaodaloja) {
+  console.log('usou o cartao da loja'), desconto1;
+} else {
+		console.log('nao usou o cartao da loja');
+	}
+}
