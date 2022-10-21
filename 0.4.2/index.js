06b7b6388d21a84dd1e47bb088e716b2773bbedb
henriqueelima <106202820+henriqueelima@users.noreply.github.com>
2022-10-21 20:29:01 -0300

    1 file added

---
+++ 0.4.2/index.js
@@ -0,0 +1,23 @@
+const alunos = [
+  {
+    nome: 'Mathias',
+    notas: [
+      { cadeira: 'DW2', nota: 10 },
+      { cadeira: 'BD2', nota: 7 },
+    ],
+  },
+  {
+    nome: 'Luiza',
+    notas: [
+      { cadeira: 'DW2', nota: 8 },
+      { cadeira: 'BD2', nota: 8 },
+    ],
+  },
+  {
+    nome: 'Pedro',
+    notas: [
+      { cadeira: 'DW2', nota: 10 },
+      { cadeira: 'BD2', nota: 10 },
+    ],
+  },
+];
